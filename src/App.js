c2ed2d8699fef8259d187307e03fa14fc4d85940
nerfic42 <bjaudall@student.42.fr>
2020-10-05 22:49:05 +0200

    first push

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+
+  const [solution, setSolution] = useState("")
+  const [nbCoup, setNbCoup] = useState(1)
+  const [message, setMessage] = useState('')
+  const [nb, setNb] = useState(Math.floor(100*Math.random()))
+  const [done, setDone] = useState(false)
+
+  const jeux = () => {
+    setNbCoup(nbCoup + 1);
+
+    if(solution < nb) { 
+      setMessage('C\'est plus!')
+    }
+    if(solution > nb) { 
+      setMessage('C\'est moins!')
+    }
+    if (solution == nb) {
+      setMessage(`Bravo! T\'as gagne en ${nbCoup} coup`)
+      setNbCoup(0)
+      setSolution('')
+      setDone(true)
+    }
+  }
+
+  const recharge = () => {
+    window.location.reload(true)
+  }
+
+  const change = (e) => {
+    setSolution(e.target.value)
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h3>Le juste prix</h3>
+      <p>Tu dois trouver le juste prix compris entre 0 et 100 en 30 secondes!</p>
+      {message}
+      <input type='number' placeholder='' value={solution} onChange={change}/>
+      <br/>
+      <button onClick={jeux}> Valider </button>
+      <br/>
+      {done ? (
+        <button onClick={recharge}>Recommencer</button>
+      ) : ''}
+      </header>
+    </div>
+  );
+}
+
+export default App;
